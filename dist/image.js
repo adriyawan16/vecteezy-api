a0359233d8bb05a31164c1ce3f63a4b4b256c03f
adriyawan16 <miftakhul.adriyawan@students.amikom.ac.id>
2023-10-18 23:49:35 +0700

    build

--- dist/image.js
+++ dist/image.js
@@ -1,59 +1,39 @@
-const https = require("https");
-function generateUrls(inputUrl) {
-  const match = inputUrl.match(/\/(\d+)-([^/]+)$/);
-  if (match) {
-    const imageID = match[1];
-    const imageName = match[2];
-    let padding = imageID.padStart(9, "0");
-    const finalID = `${padding.slice(0, 3)}/${padding.slice(3, 6)}/${padding.slice(6)}`;
-    let extensions;
-    let urlStructure;
-    if (inputUrl.includes("/photo/")) {
-      extensions = ["jpg", "jpeg", "JPG", "JPEG"];
-      urlStructure = `https://static.vecteezy.com/system/resources/previews/${finalID}/original/${imageName}-photo.`;
-    } else if (inputUrl.includes("/png/")) {
-      extensions = ["png", "PNG"];
-      urlStructure = `https://static.vecteezy.com/system/resources/previews/${finalID}/original/${imageName}-png.`;
-    } else {
-      console.log("Invalid original URL format");
-      return []; // Return an empty array if the URL format is invalid
-    }
-
-    // Generate and return the URLs as an array
-    return extensions.map(extension => `${urlStructure}${extension}`);
-  } else {
-    console.log("Invalid original URL format");
-    return []; // Return an empty array if the URL format does not match
-  }
-}
-
-function checkStatusCodes(urls) {
-  const promises = urls.map(url => {
-    return new Promise((resolve, reject) => {
-      https.get(url, {
-        timeout: 30000
-      }, res => {
-        // Check the status code
-        if (res.statusCode === 200) {
-          resolve({
-            url,
-            status: true
-          });
-        } else {
-          resolve({
-            url,
-            status: false
-          });
-        }
-      }).on("error", err => {
-        // Handle errors
-        reject(err);
+const https = require('https');
+const cheerio = require('cheerio');
+function scrapeAndCheckImageUrls(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, response => {
+      let data = '';
+      response.on('data', chunk => {
+        data += chunk;
       });
+      response.on('end', () => {
+        const $ = cheerio.load(data);
+        const filterKeyword = 'https://static.vecteezy.com/system/resources/previews';
+        const imageUrls = [];
+
+        // Assuming the image URLs are contained in img elements with specific filter keywords in their src attribute
+        $('img').each((index, element) => {
+          const imageUrl = $(element).attr('src');
+          if (imageUrl && imageUrl.includes(filterKeyword)) {
+            // Replace '/non_2x/' with '/original/'
+            const modifiedImageUrl = imageUrl.replace('/non_2x/', '/original/');
+            imageUrls.push(modifiedImageUrl);
+          }
+        });
+
+        // Resolve the promise with the modified image URLs
+        resolve(imageUrls.map(url => ({
+          url,
+          status: true
+        })));
+      });
+    }).on('error', error => {
+      // Reject the promise if there's an error
+      reject(error);
     });
   });
-  return Promise.all(promises);
 }
 module.exports = {
-  generateUrls,
-  checkStatusCodes
+  scrapeAndCheckImageUrls
 };
